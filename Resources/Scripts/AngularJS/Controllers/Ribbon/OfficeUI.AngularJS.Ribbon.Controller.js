3043061268ae5704fc5d70c7d4708c6b160153bd
Kevin De Coninck <kevindeconinck@outlook.be>
2014-12-02 15:59:21 +0100

    Added 'Collapse' on the ribbon. The function to re-show the ribbon must still be implemented.

--- Resources/Scripts/AngularJS/Controllers/Ribbon/OfficeUI.AngularJS.Ribbon.Controller.js
+++ Resources/Scripts/AngularJS/Controllers/Ribbon/OfficeUI.AngularJS.Ribbon.Controller.js
@@ -8,17 +8,53 @@
 var OfficeUIRibbon = angular.module('OfficeUIRibbon');
 
 // Defines the OfficeUIRibbon controller for the application.
-OfficeUIRibbon.controller('OfficeUIRibbon', ['$http', function($http) {
-    this.data = "demo";
+OfficeUIRibbon.controller('OfficeUIRibbon', ['$scope', '$http', function($scope, $http) {
     // Defines required variables.
     var ribbon = this;
+    
+    ribbon.hidden = false;
     
     // Get the Json file 'application.json' that defines the application data.
     $http.get('/Resources/JSon/Ribbon/ribbon.json')
         .success(function(data) {
             ribbon.Tabs = data.Tabs;
+            
+            // Sets the second tab as the active one. The second one is the tab next to the application tab.
+            ribbon.activeTab = data.Tabs[1].Id;
         })
         .error(function(data) {
             console.log('An error occured while loading the \'ribbon.json\' file.');
         });
+    
+    // Sets the currently active tab based on it's id.
+    this.setTab = function(tabId) {
+        this.activeTab = tabId;
+    }
+    
+    // Check if a given tab is active based on it's id.
+    this.isActive = function(tabId) {
+        return this.activeTab == tabId;
+    }
+    
+    $scope.changeTab = function(scrollEvent) {
+        // If we scroll up or down another event should be executed.
+        if (scrollEvent.originalEvent.detail > 0 || scrollEvent.originalEvent.wheelDelta > 0) {
+            if ($('li[role=tab].active').next().attr('id') != null) {
+                ribbon.setTab($('li[role=tab].active').next().attr('id'));
+            }
+        } else {
+            if ($('li[role=tab].active').prev().not('.application').attr('id') != null) {
+                ribbon.setTab($('li[role=tab].active').prev().attr('id'));
+            }
+        }
+        
+        $scope.$apply(); // Update the view.
+    }
+    
+    // Hide the ribbon.
+    $scope.hide = function() {
+        ribbon.hidden = true;
+        
+        $scope.$apply(); // Update the view.
+    }
 }]);
---
+++ Resources/Scripts/AngularJS/Directives/Ribbon/OfficeUI.AngularJS.Ribbon.Directive.js
@@ -0,0 +1,41 @@
+/* Loads the AngularJS module 'OfficeUI.Ribbon'. 
+   Parameters:
+        Name:           The name of the AngularJS module.
+        
+   Remarks: We're using the function 'module' of AngularJS here which accepts only 1 single parameter.
+            This signature defines that we're only loading the AngularJS module 'OfficeUI.Ribbon'.
+*/
+var OfficeUIRibbon = angular.module('OfficeUIRibbon');
+
+/* Defines a directive called 'ngcScroll'. According to AngularJS conventions, this directive must be called as 'ngc-Scroll'.
+   This directive can only be placed as an attribute and will fire when you scroll on the element which is being marked with this
+   directive. */
+OfficeUIRibbon.directive('ngcScroll', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attributes) {
+            element.on('DOMMouseScroll mousewheel', function (e) { 
+                scope.changeTab(e);
+            });
+        }
+    }
+});
+
+/* Defines a directive called 'ngcHide'. According to the AngularJS conventions, this directive must be called as 'ngc-Hide'.
+   This directive can only be placed as an attribute and will fire when you click on the element which is being marked with this 
+   directive. */
+OfficeUIRibbon.directive('ngcHide', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attributes) {
+            element.on('click', function (e) {
+                var tabContentsElement = element.parent().parent(); // Gets the 2nd parent element, this is the element hat holds the contents of the ribbon and which should be hidden.
+                
+                // Slide the tab up, so that the contents of the tab element are not visible anymore.
+                tabContentsElement.slideUp(250, function() {
+                    scope.hide();
+                });
+            });
+        }
+    }
+});
