ffebc9f9f12c13ca538dc5c7e83f85577f3111ec
Kevin De Coninck <kevindeconinck@outlook.be>
2015-01-10 15:19:10 +0100

    Removed the unnecessary files.

--- Resources/Scripts/AngularJS/Controllers/Ribbon/Components/OfficeUI.AngularJS.Ribbon.Components.Ribbon.js
+++
@@ -1,8 +0,0 @@
-/* Loads the AngularJS module 'OfficeUI.Ribbon'.
-   Parameters:
-        Name:           The name of the AngularJS module.
-        
-   Remarks: We're using the function 'module' of AngularJS here which accepts only 1 single parameter.
-            This signature defines that we're only loading the AngularJS module 'OfficeUI.Ribbon'.
-*/
-var OfficeUIRibbon = angular.module('OfficeUIRibbon');
--- Resources/Scripts/AngularJS/Modules/Ribbon/OfficeUI.AngularJS.Ribbon.Module.js
+++
@@ -1,9 +0,0 @@
-/* Defines the AngularJS module 'OfficeUIRibbon'. 
-   Parameters:
-        Name:           The name of the AngularJS module.
-        Dependencies:   The dependencies that this module does have.
-   
-   Remarks: We're using the function 'module' of AngularJS here which accepts an array of items at the end.
-            That array defines that AngularJS should create a new instance of that module.
-*/
-var OfficeUI = angular.module('OfficeUIRibbon', ['ngSanitize']);
