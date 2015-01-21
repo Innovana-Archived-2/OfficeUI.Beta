36942bdc5796c75a995cd34e6c45dc0fdb72c62e
Kevin De Coninck <kevindeconinck@outlook.be>
2015-01-21 19:48:14 +0100

    Added a new version.

--- Resources/Scripts/AngularJS/Modules/Ribbon/OfficeUI.AngularJS.Ribbon.Module.js
+++
@@ -1,9 +0,0 @@
-/* Defines the AngularJS module 'OfficeRibbon'. 
-   Parameters:
-        Name:           The name of the AngularJS module.
-        Dependencies:   The dependencies that this module does have.
-   
-   Remarks: We're using the function 'module' of AngularJS here which accepts an array of items at the end.
-            That array defines that AngularJS should create a new instance of that module.
-*/
-var OfficeUIRibon = angular.module('OfficeUIRibbon', ['ngSanitize', 'ngAnimate']);
