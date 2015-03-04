e5064d514b8627a2b155688bf3015cef40562747
kdc <kevindeconinck@outlook.be>
2015-03-04 13:56:02 +0100

    Fixed a bug in the Menu which prevents the animation from running all the times. Now the animation is runing always.

--- Resources/Scripts/AngularJS/Directives/OfficeUI/Ribbon/OfficeUI.Directive.Ribbon.Menu.js
+++ Resources/Scripts/AngularJS/Directives/OfficeUI/Ribbon/OfficeUI.Directive.Ribbon.Menu.js
@@ -15,8 +15,11 @@ OfficeUIRibbon.directive('ngcMenu', function () {
         restrict: 'A',
         link: function (scope, element, attributes) {
             element.bind("click", function(e) {
+                // Hide every open 'menu' element. This is needed because otherwise the animations will not function anymore.
+                $('.menu').each(function(index, item) {
+                    $(item).hide();
+                });
                 var menuElement = $('.menu', element.parent());
-
                 if (scope.isMenuOpened()) { $(menuElement).show('slide', { direction: 'up' }, 100); }
                 else { $(menuElement).hide(); }
             });
